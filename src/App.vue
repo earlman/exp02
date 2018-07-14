6a76aeb459b3e550677d51b255e123d522881a8f
earlman <earl.mangulabnan@gmail.com>
2018-07-14 16:55:38 -0500

    import default semantic css stylesheet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <div id="app">
-      "App" div
+    <div id="app">
+        "App" div
+        <button class="ui primary button">
+        Save
+        </button>
+        <button class="ui button">
+        Discard
+        </button>
+        <sui-button primary>Primary</sui-button>
+        <sui-button secondary>Secondary</sui-button>
       <router-view> main application content </router-view>
   </div>
 </template>
