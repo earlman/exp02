37483c569de2900229b135c591ec7b01dbcda0b2
earlman <earl.mangulabnan@gmail.com>
2018-07-14 16:04:26 -0500

    imported semantic vue and buefy libraries

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import * as firebase from 'firebase';
+import Buefy from 'buefy';
+import 'buefy/lib/buefy.css';
+import SuiVue from 'semantic-ui-vue';
+
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import {
-  store,
-} from './store';
+import { store } from './store';
+
+Vue.use(Buefy);
+Vue.use(SuiVue);
 
 Vue.config.productionTip = false;
 
@@ -16,8 +22,12 @@ new Vue({
   store,
   render: h => h(App),
   created() {
-
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDS_1RBjtJc8vVyXLgZZnbfBxojD_XExjI',
+      authDomain: 'exp02-takeoff.firebaseapp.com',
+      databaseURL: 'https://exp02-takeoff.firebaseio.com',
+      projectId: 'exp02-takeoff',
+      storageBucket: 'exp02-takeoff.appspot.com',
+    });
   },
-
-  template: '<App/>',
 });
