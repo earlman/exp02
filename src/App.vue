a9d6ecb3687d43c12b7e5ac954febac3977c5ccd
earlman <earl.mangulabnan@gmail.com>
2018-07-14 17:30:03 -0500

    sidebar navigation

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
     <div id="app">
-        "App" div
-        <button class="ui primary button">
-        Save
-        </button>
-        <button class="ui button">
-        Discard
-        </button>
-        <sui-button primary>Primary</sui-button>
-        <sui-button secondary>Secondary</sui-button>
-      <router-view> main application content </router-view>
+        <SuiSidebar animation="push" :visible="sidebar">test</SuiSidebar>
+        <div class="pusher">
+            "App" div
+            <button class="ui primary button" >
+            Save
+            </button>
+            <button class="ui button" >
+            Discard
+            </button>
+            <SuiButton primary v-on:click="sidebar=!sidebar">Primary</SuiButton>
+            <sui-button secondary>Secondary</sui-button>
+            <router-view> main application content </router-view>
+        </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+        sidebar: true,
+    };
+  }
 };
 </script>
 
